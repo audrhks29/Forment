337c64c2de776b1eedf83339e134e10b8aa6b6e2
audrhks29 <audrhks29@gmail.com>
2023-10-21 00:57:07 +0900

    Upload v1.0.0

--- src/store/productDetail-store.js
+++ src/store/productDetail-store.js
@@ -11,7 +11,7 @@ const useAppState = (set) => ({
       const [optionDataResponse, setDataResponse, detailImageResponse] = await Promise.all([
         axios.get('https://gist.githubusercontent.com/audrhks29/be6ae36bcb49942a0480aecf238cdb73/raw/919061d2b0accf458c9f62803b8016a30fa8f338/productOptionData.json'),
         axios.get('https://gist.githubusercontent.com/audrhks29/c5cc0a999cdc547ff64acb0ae1c9c905/raw/c99fcd6340d47fca7fd1025a2ebc395a22d6c134/productSetData.json'),
-        axios.get('https://gist.githubusercontent.com/audrhks29/caa13ccf491746f824fe181d779aed55/raw/46369bfe2df91393e1b19a2263c40db55f9c6957/productDetailImageData.json')
+        axios.get('https://gist.githubusercontent.com/audrhks29/caa13ccf491746f824fe181d779aed55/raw/a4daee531abd54b5ae744b5c815bddfa46ef698c/productDetailImageData.json')
       ]);
 
       set({
